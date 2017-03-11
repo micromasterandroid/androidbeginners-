127c2ad3a9c4eb1ec53caf84a8cd9297c4979bac
Byron <byronap120@gmail.com>
2017-03-11 00:45:52 -0600

    AsynkTaskExample

---
+++ Lesson_3/Exercises/AsyncTask/app/src/main/java/micromaster/galileo/edu/asynctaskexample/MainActivity.java
@@ -0,0 +1,96 @@
+package micromaster.galileo.edu.asynctaskexample;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    ArrayList<String> pokemonList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview1);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerViewAdapter = new RecyclerViewAdapter(pokemonList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+
+        final Button button = (Button) findViewById(R.id.pokemonButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new DownloadPokemonTask(MainActivity.this).execute();
+            }
+        });
+
+    }
+
+
+    private class DownloadPokemonTask extends AsyncTask<String, Integer, ArrayList<String>> {
+
+        private ProgressDialog dialog;
+
+        public DownloadPokemonTask (Context context) {
+            this.dialog =  new ProgressDialog(context);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.dialog.setMessage("Downloading data...");
+            dialog.show();
+        }
+
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+            ArrayList<String> newPokemonList = new ArrayList<>();
+            for (int i = 1; i <= 3; i++) {
+                try {
+                    Thread.sleep(1000);
+                    newPokemonList.add(simulateCalltoAPI(i));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return newPokemonList;
+        }
+
+        //This method simulate a calla to API
+        private String simulateCalltoAPI(int i){
+            switch (i) {
+                case 1:
+                    return "Bulbasaur";
+                case 2:
+                    return "Ivysaur";
+                case 3:
+                    return "Venusaur";
+                default:
+                    return "Invalid number";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            super.onPostExecute(result);
+            pokemonList.clear();
+            pokemonList.addAll(result) ;
+            recyclerViewAdapter.notifyDataSetChanged();
+            dialog.dismiss();
+        }
+    }
+}
---
+++ Lesson_3/Exercises/AsyncTask/app/src/main/java/micromaster/galileo/edu/asynctaskexample/RecyclerViewAdapter.java
@@ -0,0 +1,49 @@
+package micromaster.galileo.edu.asynctaskexample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Byron on 3/10/2017.
+ */
+
+class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<String> arrayList;
+
+    public RecyclerViewAdapter(ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView pokemonName;
+
+        ViewHolder(View view) {
+            super(view);
+            pokemonName = (TextView) view.findViewById(R.id.pokemonName);
+        }
+    }
+
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.pokemonName.setText(arrayList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+
+}
