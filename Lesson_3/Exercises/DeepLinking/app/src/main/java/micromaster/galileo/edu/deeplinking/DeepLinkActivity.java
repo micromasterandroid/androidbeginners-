e3ecb151ad97583c2422323d683f7fd55f6d939c
Byron <byronap120@gmail.com>
2017-03-15 01:09:52 -0600

    DeepLinking tutorial

---
+++ Lesson_3/Exercises/DeepLinking/app/src/main/java/micromaster/galileo/edu/deeplinking/DeepLinkActivity.java
@@ -0,0 +1,13 @@
+package micromaster.galileo.edu.deeplinking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeepLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link);
+    }
+}
